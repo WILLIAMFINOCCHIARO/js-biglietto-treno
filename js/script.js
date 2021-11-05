b44a953dbeaca5b4507092513db14310d7cec1a7
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-05 17:39:30 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Sulla base di queste informazioni dovrà calcolare 
+//  prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km) 
+// va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali,
+//    per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// Bonus: effettuate dei controlli sui dati di input.
