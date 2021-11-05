1cc71fd3512b882eeddf3c7d4f8b8ee33361efd0
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-05 18:21:58 +0100

    initial

--- js/script.js
+++ js/script.js
@@ -6,12 +6,32 @@
 //    per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 // Bonus: effettuate dei controlli sui dati di input.
 
-let minorenne = (prezzobiglietto * 20 / 100 );
-let over65 = (prezzobiglietto * 40 / 100);
+const regione = prompt('Dove vai con il treno?');
+
+document.getElementById ('regione').innerHTML = (regione);
+
+let milano = 200;
+let napoli = 300;
+let roma = 400;
+let firenze = 500;
+let catania = 600;
+let Catanzaro = 700;
+let Bologna = 600;
+
+
 
 let km = 1;
+ 
+let prezzobiglietto = (0,21 * km);
+console.log(prezzobiglietto);
 
-const prezzobiglietto = (0,21 * km);
-console.log(prezzobiglietto)
+let minorenne = (prezzobiglietto * 20 / 100 );
+console.log(minorenne);
 
-if ()
+let over65 = (prezzobiglietto * 40 / 100);
+console.log(over65);
+
+
+
+document.getElementById ('prezzo').innerHTML = ('il prezzo del biglietto è :'(prezzofinale))
+
